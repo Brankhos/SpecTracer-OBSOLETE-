25ac4322f5a7e4d64b74752016045585324e5ee0
Brankhos <76525771+Brankhos@users.noreply.github.com>
2022-01-27 00:17:24 +0300

    .

---
+++ files/install.py
@@ -0,0 +1,26 @@
+import os, platform, subprocess, re, sys
+
+
+def get_processor_name():
+    if platform.system() == "Windows":
+        return platform.processor()
+    elif platform.system() == "Darwin":
+        os.environ['PATH'] = os.environ['PATH'] + os.pathsep + '/usr/sbin'
+        command = "sysctl -n machdep.cpu.brand_string"
+        return subprocess.check_output(command).strip()
+    elif platform.system() == "Linux":
+        command = "cat /proc/cpuinfo"
+        all_info = subprocess.check_output(command, shell=True).strip()
+        for line in all_info.split("\n"):
+            if "model name" in line:
+                return re.sub(".*model name.*:", "", line, 1)
+    return ""
+
+loca = os.path.join(os.path.dirname(__file__))
+
+if "Intel" in get_processor_name():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", loca + '\mrequ-i.txt'])
+
+else:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", loca + '\mrequ-a.txt'])
+
